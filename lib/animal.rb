b90afa9fd31a2eb4ea31af705d97fcd45d75fd69
Student <example@example.com>
2019-07-30 11:47:41 -0700

    add class

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,1 +1,13 @@
+class Shelter
+  attr_accessor: :species, :name, :breed, :gender, :timestamp, :id
 
+  def initialize(attr)
+    @species = attr.fetch(:species)
+    @name = attr.fetch(:name)
+    @breed = attr.fetch(:breed)
+    @gender = attr.fetch(:gender)
+    @timestamp = attr.fetch(:time)
+    @id = attr.fetch(:id)
+  end
+
+end
